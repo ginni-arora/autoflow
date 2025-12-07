c8bf450049e2ea92807b0d0addf3dbee38765984
Ginni Arora <ginnia934@gmail.com>
2025-12-07 19:12:20 +0530

    12-workflows-pagination

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,14 +1,21 @@
 import { WorkflowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
+import { workflowsParamsLoader } from "@/features/workflows/server/params-loader";
 import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
 import { HydrateClient } from "@/trpc/routers/server";
 import { ErrorBoundary } from "@sentry/nextjs";
+import type { SearchParams } from "nuqs/server";
 import { Suspense } from "react";
 
-const Page = async () => {
-  await requireAuth();
+type Props = {
+  searchParams: Promise<SearchParams>
+}
 
-  prefetchWorkflows();
+const Page = async ( { searchParams }: Props) => {
+  await requireAuth();
+  const params = await workflowsParamsLoader(searchParams);
+
+  prefetchWorkflows(params);
 
   return (
     <WorkflowsContainer>
--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -2,14 +2,16 @@ import { useTRPC } from "@/trpc/client";
 import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { useWorkflowsParams } from "./use-workflows-params";
 
 /**
  * Hook to fetch all workflows using suspense
  */
 export const useSuspenseWorkflows = () => {
   const trpc = useTRPC();
+  const [params] = useWorkflowsParams();
 
-  return useSuspenseQuery(trpc.workflows.getMany.queryOptions());
+  return useSuspenseQuery(trpc.workflows.getMany.queryOptions(params));
 };
 
 /**
@@ -27,7 +29,7 @@ export const useCreateWorkflow =() => {
                 toast.success(`Workflow "${data.name}" created;`)
                 
                 queryClient.invalidateQueries(
-                    trpc.workflows.getMany.queryOptions(),
+                    trpc.workflows.getMany.queryOptions({}),
                 );
             },
             onError:(error) =>{
